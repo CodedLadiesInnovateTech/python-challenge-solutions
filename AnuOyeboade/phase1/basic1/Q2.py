ebfcac7c0de9f7da59e8dab36571947d78922373
Anuoluwapo Oyeboade <oyeboadeanuoluwapo@gmail.com>
2020-05-13 09:14:42 +0100

    Day 2 Python Challenge

---
+++ AnuOyeboade/phase1/basic1/Q2.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to get the Python version you are using
+"""
+import sys
+print("Python version")
+print(sys.version)
---
+++ AnuOyeboade/phase1/basic1/Q5.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+"""
+
+first = input("First name: ")
+last = input("Last name:")
+print(last+" "+first)
