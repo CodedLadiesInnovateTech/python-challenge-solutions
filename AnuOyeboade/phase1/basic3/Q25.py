eba118a6c0b15288f142aec20e4d2e06da89fdc8
Anuoluwapo Oyeboade <oyeboadeanuoluwapo@gmail.com>
2020-05-14 13:50:17 +0100

    Day 4 Python Challenge

---
+++ AnuOyeboade/phase1/basic3/Q25.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to check whether a specified value is contained in a group of values.
+Test Data :
+3 -> [1, 5, 8, 3] : True
+-1 -> [1, 5, 8, 3] : False
+"""
+list = [1,5,8,3]
+print(3 in list)
+print(-1 in list)
+
