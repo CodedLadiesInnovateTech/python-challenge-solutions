70291be08ee94faec1a2ddd8bc766810eeb0b7ee
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2020-05-14 11:32:18 +0100

    Day2 solutions

---
+++ Day2/Q5.py
@@ -0,0 +1,3 @@
+first_name = input('What is your first name?')
+last_name = input('What is your surname')
+print(last_name + ' ' + first_name)
---
+++ Day2/Q7.py
@@ -0,0 +1,3 @@
+file = input('Input name of file')
+extension = file.split('.')
+print('The extension of the file is :' + extension[-1]) 
