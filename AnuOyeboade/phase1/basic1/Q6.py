4d8f16be8db706117592bab8c67c9197695dc62b
Clare-Uche <58085127+Clare-Uche@users.noreply.github.com>
2020-05-14 10:09:34 +0100

    created a file Task-1

--- AnuOyeboade/phase1/basic1/Q6.py
+++ AnuOyeboade/phase1/basic1/Q6.py
@@ -1,4 +1,4 @@
-"""
+33"""
 Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
 Sample data : 3, 5, 7, 23
 Output :
--- neoOkpara/Phase-1/Python Basic 1/nMaths.py
+++ neoOkpara/Phase-1/Python Basic 1/nMaths.py
@@ -1,8 +1,7 @@
 import math
 
-n = raw_input("Accepts integer: ")
+n = input("Accepts integer: ")
 intN = int(n)
-
 result = intN * 123
 print(result)
 
