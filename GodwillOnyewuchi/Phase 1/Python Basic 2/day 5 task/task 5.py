d6a80fc44653686828511128deefdcb45bb2948a
GREENFONTS <godwillonyewuchii@gmail.com>
2020-05-15 20:46:19 +0100

    Day 5

---
+++ GodwillOnyewuchi/Phase 1/Python Basic 2/day 5 task/task 5.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+if num1 == num2 or num1 + num2 == 5 or num1 - num2 == 5:
+    print("True")
+else:
+    print("False")
