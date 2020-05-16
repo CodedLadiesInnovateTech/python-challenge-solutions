fd3040c7e673b90d4bd90021cb81c954812debb6
obasi <obasiemmanuel28@gmail.com>
2020-05-16 04:19:43 -0700

    Day 3 task

---
+++ ObasiEmmanuel/Phase 1/Python Basic 1/Day 3 task/day3(2).py
@@ -0,0 +1,7 @@
+import calendar
+print(calendar.month(2020,5))
+print(calendar.weekheader(10))
+print(calendar.weekday(2020,5,15))
+print(calendar.monthcalendar(2020,5))
+print(calendar.calendar(2020))
+
---
+++ ObasiEmmanuel/Phase 1/Python Basic 1/Day 3 task/day3(6).py
@@ -0,0 +1,7 @@
+import math
+number=float(input("Enter number: "))
+if number > 17:
+    solution = 2*(abs(17-number))
+else:
+    solution=(abs(17-number))
+print(solution)
