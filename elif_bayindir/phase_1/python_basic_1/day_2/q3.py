7f857d295fa6f59924b536fde322ed83c19648e0
elif <elif.bayindir1@gmail.com>
2020-05-17 13:06:27 +0300

    Day2 solutions

---
+++ elif_bayindir/phase_1/python_basic_1/day_2/q3.py
@@ -0,0 +1,10 @@
+# Question 3
+
+# Learn current date and time
+
+from datetime import datetime 
+
+current = datetime.now()
+print ("Current date and time :", current.strftime("%Y-%m-%d %H:%M:%S")) # year/month/day hour:minutes:seconds
+
+
