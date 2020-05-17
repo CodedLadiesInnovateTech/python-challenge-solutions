de67ecc0fcae1376a9de43336d5c1c12efd7d46f
Clare-Uche <58085127+Clare-Uche@users.noreply.github.com>
2020-05-17 23:40:49 +0100

    Day 2 and Day 3 solutions

--- Task-10.py
+++
@@ -1,6 +0,0 @@
-text= input('Enter String:')
-copies=int(input('How many times:'))
-n= " "
-for i in range(copies):
-    n = n + text
-    print(n)
--- Task-9.py
+++
@@ -1,5 +0,0 @@
-new_string= str(input('Enter new string:'))
-if new_string [0:2]=='Is':
- print(new_string)
-else:
-     print('Is '+ new_string )
