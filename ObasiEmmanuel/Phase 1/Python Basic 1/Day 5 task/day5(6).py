e778da38d46c5a5d4310fc0175b93d23481684f1
obasi <obasiemmanuel28@gmail.com>
2020-05-17 23:33:25 -0700

    Day 5 task

---
+++ ObasiEmmanuel/Phase 1/Python Basic 1/Day 5 task/day5(6).py
@@ -0,0 +1,13 @@
+def summation(a,b):
+    c=type(a)==int
+    d=type(b)==int
+    if c is True and d is True:
+        print(a+b)
+    else:
+        return print("numbers not integer!")
+
+
+
+
+
+summation (10.4,3)
---
+++ ObasiEmmanuel/Phase 1/Python Basic 1/Day 5 task/day5(7).py
@@ -0,0 +1,6 @@
+def details(name,age,address):
+    return print("your name:",name,"\nyour age:",age,"\nyour address:",address,"\n")
+
+
+
+details("obasi",27,"no 20 freedom street bwar Abuja")
