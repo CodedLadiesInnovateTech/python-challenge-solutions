97a6086a466621d676fb25e166fc01bee35e330a
Ekeopara-Praise <60721962+Ekeopara-Praise@users.noreply.github.com>
2020-05-20 23:06:39 +0100

    Day9 solution

---
+++ Ekeopara_Praise/Phase 1/Python Basic 1/Day9 Tasks/Task8.py
@@ -0,0 +1,5 @@
+'''8. Write a Python program to find the available built-in modules.'''
+import sys
+import textwrap
+module_name = ', '.join(sorted(sys.builtin_module_names))
+print(textwrap.fill(module_name, width=70))
