ebdc0324d423ac4e5217a4c17af226336c3ce0e5
tmannazy <e.anene@hotmail.com>
2020-05-20 11:46:27 +0100

    Day7  Challenge Solutions.

---
+++ Emmanuel ANENE/Phase 1/Python Basic 1/Day7/task2.py
@@ -0,0 +1,7 @@
+'''
+Write a Python program to print to stderr.
+'''
+
+import sys
+
+print(sys.stderr)
---
+++ Emmanuel ANENE/Phase 1/Python Basic 1/Day7/task7.py
@@ -0,0 +1,7 @@
+'''
+Write a program to get execution time for a Python method
+'''
+
+import os
+
+print("The execution time is:", os.times())
