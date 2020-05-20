a23b998ebf021498f4721095621ec4ab66148dab
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2020-05-20 07:10:03 +0100

    solutions

---
+++ MotunrayoKoyejo/Phase 1/Python Basic 1/Day7/Q56.py
@@ -0,0 +1,8 @@
+def terminal_size():
+    import fcntl, termios, struct
+    th, tw, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+    return tw, th
+
+print('Number of columns and Rows: ',terminal_size())
---
+++ MotunrayoKoyejo/Phase 1/Python Basic 1/Day7/Q60.py
@@ -0,0 +1,7 @@
+from math import sqrt
+print("Input lengths of shorter triangle sides:")
+a = float(input("a: "))
+b = float(input("b: "))
+
+c = sqrt(a**2 + b**2)
+print("The length of the hypotenuse is", c )
