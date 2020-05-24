eb66f3f8814c6a3738a14ec45c1c16207fbe1c48
elif <elif.bayindir1@gmail.com>
2020-05-25 00:06:11 +0300

    Day7 Solutions

---
+++ elif_bayindir/phase_1/python_basic_1/day_7/q2.py
@@ -0,0 +1,6 @@
+# Question 2
+# Print to stderr.
+
+import sys
+
+print("Print to stderr", file=sys.stderr)
---
+++ elif_bayindir/phase_1/python_basic_1/day_7/q4.py
@@ -0,0 +1,8 @@
+# Question 4
+# Get the current username
+
+import getpass
+print(getpass.getuser())
+
+import os
+print(os.getlogin())
