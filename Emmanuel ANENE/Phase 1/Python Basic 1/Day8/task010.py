72653dc468a72df79ab15ee31b9670bbafa391b5
tmannazy <e.anene@hotmail.com>
2020-05-25 16:00:26 +0100

    Day8 Challenge Solutions Modified.

--- Emmanuel ANENE/Phase 1/Python Basic 1/Day8/task010.py
+++ Emmanuel ANENE/Phase 1/Python Basic 1/Day8/task010.py
@@ -1,3 +1,9 @@
 '''
 Write a Python program to sort files by date.
 '''
+
+import glob, os
+
+data = glob.glob("*.py")
+data.sort(key=os.path.getctime)
+print("\n".join(data))
--- Emmanuel ANENE/Phase 1/Python Basic 1/Day8/task7.py
+++ Emmanuel ANENE/Phase 1/Python Basic 1/Day8/task7.py
@@ -2,3 +2,17 @@
 Write a Python program to convert pressure in kilopascals to pounds per square inch, a millimeter of mercury (mmHg) and atmosphere pressure.
 '''
 
+# kPa to psi = 0.145
+# kPa to mmHg = 7.50062
+# kPa to atp = 0.00987
+
+val = round(float(input("Enter a value: ")), 0)
+kpaUnit = "kPa"
+print(int(val), kpaUnit)
+
+calPsi =  round(val * 0.145, 3)
+calMmHg = round( val * 7.50062, 3)
+calAtp =  round(val * 0.00987, 3)
+
+print("{} conversions: {} psi, {} mmHg, and {} atp.".format(val, calPsi, calMmHg, calAtp))
+
