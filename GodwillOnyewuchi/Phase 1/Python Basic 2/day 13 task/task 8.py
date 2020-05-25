e9278db419d8ead66f55e4f82619240a82ada600
GREENFONTS <godwillonyewuchii@gmail.com>
2020-05-25 16:49:53 +0100

    day 13 task

---
+++ GodwillOnyewuchi/Phase 1/Python Basic 2/day 13 task/task 8.py
@@ -0,0 +1,6 @@
+# Python program to create a bytearray from a list
+
+nums = [10, 20, 56, 35, 17, 99]
+values = bytearray(nums)
+for x in values: 
+    print(x)
---
+++ GodwillOnyewuchi/Phase 1/Python Basic 2/day 13 task/task10.py
@@ -0,0 +1,7 @@
+# Python program to format a specified string to limit the number of characters to 6
+
+String = 'impossible'
+
+Output = String[0:6]
+
+print(Output)
