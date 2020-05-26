4923d8a9111fb8263d96f30a141b262b5097907c
chibuzor darlington <darlingtonchibuzor64@gmail.com>
2020-05-26 13:19:51 +0100

    darlington day 13 solution submission

---
+++ Darlington/phase1/python Basic 1/day 13 solution/qtn9.py
@@ -0,0 +1,5 @@
+# program to display a floating number in specified numbers.
+order_amt = 212.374
+print('\nThe total order amount comes to %f' % order_amt)
+print('The total order amount comes to %.2f' % order_amt)
+print()
