df01047b853f01b39728bb2e43b06fc543f59efa
GREENFONTS <godwillonyewuchii@gmail.com>
2020-05-26 08:02:31 +0100

    init

--- GodwillOnyewuchi/Phase 1/Python Basic 2/day 13 task/task 1.py
+++ GodwillOnyewuchi/Phase 1/Python Basic 2/day 13 task/task 1.py
@@ -1,4 +1,4 @@
-# Python program to make file lists from current directory using a wildcard
+# Python program to make file lists from current directory using a wildcard..
 
 import glob
 file_list = glob.glob('*.*')
