ac39037ed96d8801aa3a631e09590e6ef34f456f
Emmanuel Nwabudo <emmox55@gmail.com>
2020-05-27 09:02:08 +0100

    Solution for Day 11

---
+++ neoOkpara/Phase-1/Day11/defineChar.py
@@ -0,0 +1,8 @@
+print("\#{'}${\"}@/")
+print("\#{'}${"'"'"}@/")
+print(r"""\#{'}${"}@/""")
+print('\#{\'}${"}@/')
+print('\#{'"'"'}${"}@/')
+print(r'''\#{'}${"}@/''')
+
+# https://www.w3resource.com/python-exercises/python-basic-exercise-92.php
