40866b0c242341e816c4ae3e99483a91d34a67ec
chibuzor darlington <darlingtonchibuzor64@gmail.com>
2020-05-27 16:02:44 +0100

    darlington day 15 solution submission

---
+++ Darlington/phase1/python Basic 1/day 15 solution/qtn1.py
@@ -0,0 +1,7 @@
+#program to split a variable length string into variables.
+var_list = ['a', 'b', 'c']
+x, y, z = (var_list + [None] * 3)[:3]
+print(x, y, z)
+var_list = [100, 20.25]
+x, y = (var_list + [None] * 2)[:2]
+print(x, y)
