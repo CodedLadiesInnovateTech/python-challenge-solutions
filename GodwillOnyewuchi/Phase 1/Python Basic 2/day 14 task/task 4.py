ab0dbe40c76125199354daf52c08cfefbdfe8df8
GREENFONTS <godwillonyewuchii@gmail.com>
2020-05-31 19:42:03 +0100

    day 14 task

---
+++ GodwillOnyewuchi/Phase 1/Python Basic 2/day 14 task/task 4.py
@@ -0,0 +1,11 @@
+# Write a Python program to check whether multiple variables have the same value
+
+a = 4
+b = 5
+c = 20
+d = 20
+
+if a == b == c == d:
+    print('variables have the same value')
+else:
+    print("different values")
