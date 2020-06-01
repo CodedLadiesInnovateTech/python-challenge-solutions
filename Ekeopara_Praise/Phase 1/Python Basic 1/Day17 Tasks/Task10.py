a97f3baa9ad691190531527a4abb134c50042808
Ekeopara-Praise <60721962+Ekeopara-Praise@users.noreply.github.com>
2020-06-01 11:32:22 +0100

    Day17 Solution

---
+++ Ekeopara_Praise/Phase 1/Python Basic 1/Day17 Tasks/Task10.py
@@ -0,0 +1,16 @@
+'''10. Write a Python program to find the number of zeros at the end of a factorial of a given positive number.
+Range of the number(n): (1 = n = 2*109).'''
+
+def factendzero(n):
+  x = n // 5
+  y = x 
+  while x > 0:
+    x /= 5
+    y += int(x)
+  return y
+       
+print(factendzero(5))
+print(factendzero(12))
+print(factendzero(100))
+
+#Reference: wresource 
---
+++ Ekeopara_Praise/Phase 1/Python Basic 1/Day17 Tasks/Task9.py
@@ -0,0 +1,15 @@
+'''9. Write a Python program to find the value of n where n degrees of number 2 are written sequentially in a line without spaces.'''
+
+def ndegrees(num):
+  ans = True
+  n, tempn, i = 2, 2, 2
+  while ans:
+    if str(tempn) in num:
+      i += 1
+      tempn = pow(n, i)
+    else:
+      ans = False
+  return i-1;
+print(ndegrees("2481632"))
+print(ndegrees("248163264"))
+#Reference: wresource 
