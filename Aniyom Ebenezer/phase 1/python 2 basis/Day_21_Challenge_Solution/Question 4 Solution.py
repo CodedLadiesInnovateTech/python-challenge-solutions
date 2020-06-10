891ec7047d9f4a29662d149018de000de7d1045a
eaniyom@gmail.com <eaniyom@gmail.com>
2020-06-10 04:36:50 -0500

    Day_21_Challenge_Solution

---
+++ Aniyom Ebenezer/phase 1/python 2 basis/Day_21_Challenge_Solution/Question 4 Solution.py
@@ -0,0 +1,24 @@
+"""
+Write a Python program to find the maximum sum of a contiguous subsequence from a given sequence of numbers
+a1, a2, a3,... an. A subsequence of one element is also a continuous subsequence.
+"""
+while True:
+    print("Input number of sequence of numbers you want to input (0 to exit): ")
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        A = []
+        Sum = []
+        print("Input numbers: ")
+        for i in range(n):
+            A.append(int(input()))
+        Wa = 0
+        for i in range(0, n):
+            Wa += A[i]
+            Sum.append(Wa)
+        for i in range(0, n):
+            for j in range(0, i):
+                Num = Sum[i] - Sum[j]
+                Sum.append(Num)
+        print("Maximum sum of the said contiguous subsequence: ", max(Sum))
---
+++ Aniyom Ebenezer/phase 1/python 2 basis/Day_21_Challenge_Solution/Question 7 Solution.py
@@ -0,0 +1,18 @@
+"""
+Write a Python program which reads a text(Only alphabetical characters and spaces.) and prints two words.
+The first one is the word which is arise most frequently in the text. The second one is the word which
+has the maximum number of letters.
+
+Note: A word is a sequence of letters which is separated by the spaces.
+"""
+import collections
+print("Input a text in a line: ")
+text_list = list(map(str, input().split()))
+sc = collections.Counter(text_list)
+common_word = sc.most_common()[0][0]
+max_char = ""
+for s in text_list:
+    if len(max_char) < len(s):
+        max_char = s
+print("\nMost frequent text and the word which has maximum number of letters.")
+print(common_word, max_char)
