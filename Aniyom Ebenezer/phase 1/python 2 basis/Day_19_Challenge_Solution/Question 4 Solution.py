49c0409f37e45c7d7be1e04d87348d9f09757f49
eaniyom@gmail.com <eaniyom@gmail.com>
2020-06-10 04:45:17 -0500

    Day_18_Challenge_solution

--- Aniyom Ebenezer/phase 1/python 2 basis/Day_19_Challenge_Solution/Question 4 Solution.py
+++
@@ -1,7 +0,0 @@
-"""
-Write a Python program to find the number of divisors of a given integer is even or odd
-"""
-n = int(input("Input a number: "))
-for i in range(n):
-    x = len([i for i in range(1, n+1) if not n % i])
-print(x)
