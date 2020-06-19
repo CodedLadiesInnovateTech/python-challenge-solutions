6a63f26f55531794feb0f3b6d6d9882c272746c1
Ekeopara-Praise <60721962+Ekeopara-Praise@users.noreply.github.com>
2020-06-19 23:30:52 +0100

    Day29 Solution

---
+++ Ekeopara_Praise/Phase 2/STRINGS/Day29 Tasks/Task4.py
@@ -0,0 +1,14 @@
+'''4. Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$',
+ except the first char itself.
+    Sample String : 'restart'
+    Expected Result : 'resta$t'''
+def change_char(str1):
+  char = str1[0]
+  str1 = str1.replace(char, '$')
+  str1 = char + str1[1:]
+
+  return str1
+
+print(change_char('restart'))
+
+#Reference: w3resource
---
+++ Ekeopara_Praise/Phase 2/STRINGS/Day29 Tasks/Task6.py
@@ -0,0 +1,22 @@
+'''6. Write a Python program to add 'ing' at the end of a given string (length should be at least 3). If the given string already 
+ends with 'ing' then add 'ly' instead. If the string length of the given string is less than 3, leave it unchanged.
+    Sample String : 'abc'
+    Expected Result : 'abcing'
+    Sample String : 'string'
+    Expected Result : 'stringly' '''
+
+def add_string(str1):
+  length = len(str1)
+
+  if length > 2:
+    if str1[-3:] == 'ing':
+      str1 += 'ly'
+    else:
+      str1 += 'ing'
+
+  return str1
+print(add_string('ab'))
+print(add_string('abc'))
+print(add_string('string'))
+
+#Reference: w3resource
