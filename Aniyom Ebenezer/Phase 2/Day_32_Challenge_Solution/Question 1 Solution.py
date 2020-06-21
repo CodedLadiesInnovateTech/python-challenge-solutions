f73fcb3af841c1d023458643d163bcd27a930b42
eaniyom@gmail.com <eaniyom@gmail.com>
2020-06-21 02:07:43 -0500

    Day_32_Challenge_Solution

---
+++ Aniyom Ebenezer/Phase 2/Day_32_Challenge_Solution/Question 1 Solution.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program to print the following floating numbers upto 2 decimal places with a sign.
+"""
+x = 9.7576
+print("Original number is: {}".format(x))
+print()
+print("Formatted Number with sign is: #{:.2f}k".format(x))
---
+++ Aniyom Ebenezer/Phase 2/Day_32_Challenge_Solution/Question 6 Solution.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program to format a number with a percentage.
+"""
+x = 0.5
+print("Original Number is: {}".format(x))
+print()
+print("Formatted Number is: {:.2%}".format(x))
