3560dd55709633c35b8c74000f1e0c1842c07020
Uche-Clare <clareuche97@gmail.com>
2020-06-23 10:07:11 +1000

    Day 14 and 15

---
+++ Uche Clare/Phase 1/Python Basic 1/Day 14/Task 3.py
@@ -0,0 +1,8 @@
+'''Write a Python program to determine the largest and smallest integers, longs, floats.'''
+import sys
+print()
+print('Integer information: ', sys.int_info)
+print()
+print('Float information: ', sys.float_info)
+print()
+print('Longs Information: ', sys.maxsize)
---
+++ Uche Clare/Phase 1/Python Basic 1/Day 15/task 4.py
@@ -0,0 +1,2 @@
+x, y = map(int, input('Values:  ').split())
+print("The value of x & y are: ",{x,} , {y})
