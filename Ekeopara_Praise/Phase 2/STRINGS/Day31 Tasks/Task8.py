8323063a1e66633ebeaa1e28f38a85e955da70bb
Ekeopara-Praise <ekeoparapraise@gmail.com>
2020-06-27 20:23:59 +0100

    Day31 Task Solution

---
+++ Ekeopara_Praise/Phase 2/STRINGS/Day31 Tasks/Task8.py
@@ -0,0 +1,19 @@
+'''8. Write a Python program to add a prefix text to all of the lines in a string.''' 
+
+import textwrap
+sample_text ='''
+    Python is a widely used high-level, general-purpose, interpreted,
+    dynamic programming language. Its design philosophy emphasizes
+    code readability, and its syntax allows programmers to express
+    concepts in fewer lines of code than possible in languages such
+    as C++ or Java.
+    '''
+text_without_Indentation = textwrap.dedent(sample_text)
+wrapped = textwrap.fill(text_without_Indentation, width=50)
+#wrapped += '\n\nSecond paragraph after a blank line.'
+final_result = textwrap.indent(wrapped, '> ')
+print()
+print(final_result)
+print()
+
+#Reference: w3resource
