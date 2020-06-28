5c58d402f38a28ba90b130c2509bd1ff69ca0532
eaniyom@gmail.com <eaniyom@gmail.com>
2020-06-28 15:59:30 -0500

    Day_36_Challenge_Solution

---
+++ Aniyom Ebenezer/Phase 2/Day_36_Challenge_Solution/Question 4 Solution.py
@@ -0,0 +1,27 @@
+"""
+Write a Python program to find the minimum window in a given string which will contain all the characters of another given string.
+Example 1
+Input : str1 = " PRWSOERIUSFK "
+str2 = " OSU "
+Output: Minimum window is "OERIUS"
+"""
+import collections
+def min_window(str1, str2):
+    result_char, missing_char = collections.Counter(str2), len(str2)
+    i = p = q = 0
+    for j, c in enumerate(str1, 1):
+        missing_char -= result_char[c] > 0
+        result_char[c] -= 1
+        if not missing_char:
+            while i < q and result_char[str1[i]] < 0:
+                result_char[str1[i]] += 1
+                i += 1
+            if not q or j - i <= q - p:
+                p, q = i, j
+    return str1[p:q]
+           
+str1 = "PRWSOERIUSFK"
+str2 = "OSU"
+print("Original Strings:\n",str1,"\n",str2)
+print("Minimum window:")
+print(min_window(str1,str2))
---
+++ Aniyom Ebenezer/Phase 2/Day_36_Challenge_Solution/Question 7 Solution.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to count number of non-empty substrings of a given string.
+"""
+def number_of_substrings(str): 
+    str_len = len(str) 
+    return int(str_len * (str_len + 1) / 2)
+str1 = input("Input a string: ")
+print("Number of substrings:") 
+print(number_of_substrings(str1))
