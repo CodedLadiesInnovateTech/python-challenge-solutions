cf1070925cd2f91dfd3c2bd36ba605750d58fe42
Ekeopara-Praise <ekeoparapraise@gmail.com>
2020-06-28 20:10:39 +0100

    Day33 Task Solution

---
+++ Ekeopara_Praise/Phase 2/STRINGS/Day33 Tasks/Task10.py
@@ -0,0 +1,8 @@
+''''10. Write a Python program to split a string on the last occurrence of the delimiter.'''
+
+str1 = "w,3,r,e,s,o,u,r,c,e"
+print(str1.rsplit(',', 1))
+print(str1.rsplit(',', 2))
+print(str1.rsplit(',', 5))
+
+#Reference: w3resource
