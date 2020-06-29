6921d304e7cb65cde806884df46ce804bb52433f
Ekeopara-Praise <ekeoparapraise@gmail.com>
2020-06-29 23:31:30 +0100

    Day40 Task Solution

---
+++ Ekeopara_Praise/Phase 2/LIST/Day40 Tasks/Task5.py
@@ -0,0 +1,6 @@
+'''5. Write a Python program to shuffle and print a specified list. '''
+
+import random
+lst = ['1', '2', '3', '4', '5', '6']
+random.shuffle(lst)
+print(lst) 
---
+++ Ekeopara_Praise/Phase 2/LIST/Day40 Tasks/Task8.py
@@ -0,0 +1,4 @@
+'''8. Write a Python program to generate all permutations of a list in Python. '''
+
+import itertools
+print(list(itertools.permutations([1,2,3])))
