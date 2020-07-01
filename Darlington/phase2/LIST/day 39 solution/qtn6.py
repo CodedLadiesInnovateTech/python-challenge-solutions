883171d106a57d53f33cafed5309bc6ba50cdcb8
chibuzor darlington <darlingtonchibuzor64@gmail.com>
2020-07-01 15:23:45 +0100

    darlington day 39 solution submission

---
+++ Darlington/phase2/LIST/day 39 solution/qtn6.py
@@ -0,0 +1,8 @@
+#program to get a list, sorted in increasing order by the 
+#last element in each tuple from a given list of non-empty tuples.
+def last(n): return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
