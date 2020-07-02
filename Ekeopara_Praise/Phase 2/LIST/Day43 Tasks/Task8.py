278686e8a6e2e14865ad7b0b63e2dc2b3eaa1170
Ekeopara-Praise <ekeoparapraise@gmail.com>
2020-07-02 10:33:10 +0100

    Day43 Task Solution

---
+++ Ekeopara_Praise/Phase 2/LIST/Day43 Tasks/Task8.py
@@ -0,0 +1,5 @@
+'''8. Write a Python program to print a nested lists (each list on a new line) using the print() function. '''
+
+nestedList = [[1,2,3], [4,5,6], [0,9,8]]
+for i in nestedList:
+    print(i)
