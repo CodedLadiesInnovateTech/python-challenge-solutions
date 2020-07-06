04d946382aa38db92814953fed889558ebe064e9
Uche-Clare <clareuche97@gmail.com>
2020-07-07 00:28:20 +1000

    Day 16,17,29 and 30

---
+++ Uche Clare/Phase 2/String/Day 29/Task 1.py
@@ -0,0 +1,5 @@
+#Write a Python program to calculate the length of a string.
+
+String = str(input('Enter String: '))
+print()
+print('The Length of the String is: ', len(String))    #Length of a string
