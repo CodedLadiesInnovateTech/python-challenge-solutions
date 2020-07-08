6849777f7b4e009047de03ee760ff10d9e0b0f1f
Uche-Clare <clareuche97@gmail.com>
2020-07-09 07:26:05 +1000

    Day 33

---
+++ Uche Clare/Phase 2/String/Day 33/Task 5.py
@@ -0,0 +1,9 @@
+# Write a Python program to check if a string contains all letters of the alp
+
+
+import string
+alph_string = "the quick brown fox jumps over the lazy dog"
+if alph_string >= string.ascii_lowercase:
+    print("True")
+else:
+    print("False")
---
+++ Uche Clare/Phase 2/String/Day 33/Task 6.py
@@ -0,0 +1,4 @@
+#Write a Python program to convert a string in a list.
+string = "Python is the easiest programming language"
+str_list_cvtr = string.strip().split()
+print(str_list_cvtr)
