b259c081b534c51c2fa671116ff0c94971baf7d4
Uche-Clare <clareuche97@gmail.com>
2020-07-10 07:27:46 +1000

    Day 34

--- Aniyom Ebenezer/Phase 2/STRINGS/Day_34_Challenge_Solution/Question 3 Solution.py
+++ Aniyom Ebenezer/Phase 2/STRINGS/Day_34_Challenge_Solution/Question 3 Solution.py
@@ -6,4 +6,4 @@ def first_repeated_char(str1):
         if str1[:index + 1].count(c)>1:
             return c
     return "None"
-print(first_repeated_char("abcdabcd"))
+print(first_repeated_char("abcdbcd"))
