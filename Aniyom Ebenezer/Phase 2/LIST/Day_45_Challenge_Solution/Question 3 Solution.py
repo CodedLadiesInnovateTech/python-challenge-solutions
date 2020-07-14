99874082ebd2748a470c66f3de76109b8260b189
eaniyom@gmail.com <eaniyom@gmail.com>
2020-07-14 12:02:07 -0500

    Day_45_Challege_Solution

---
+++ Aniyom Ebenezer/Phase 2/LIST/Day_45_Challenge_Solution/Question 3 Solution.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "['empty1', 'empty2', 'empty3', 'empty4', 'empty5']\n"
+     ]
+    }
+   ],
+   "source": [
+    "\"\"\"\n",
+    "Write a Python program to insert a given string at the beginning of all items in a list\n",
+    "\"\"\"\n",
+    "\n",
+    "num = [1, 2, 3, 4, 5]\n",
+    "print([\"empty{0}\".format(i) for i in num])"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
